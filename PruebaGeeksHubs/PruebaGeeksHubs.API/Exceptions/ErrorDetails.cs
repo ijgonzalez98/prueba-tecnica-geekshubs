834e1ef9afa922c3175082cc7e3e52650a717345
Iván Jiménez González <ivanjimenez.contact@gmail.com>
2022-08-30 01:19:40 +0200

    Añadir control de excepciones simple

---
+++ PruebaGeeksHubs/PruebaGeeksHubs.API/Exceptions/ErrorDetails.cs
@@ -0,0 +1,14 @@
+﻿namespace PruebaGeeksHubs.API.Exceptions
+{
+    internal class ErrorDetails
+    {
+        public ErrorDetails(int statusCode, string message)
+        {
+            StatusCode = statusCode;
+            Message = message;
+        }
+
+        public int StatusCode { get; set; }
+        public string Message { get; set; }
+    }
+}
---
+++ PruebaGeeksHubs/PruebaGeeksHubs.API/Exceptions/ExceptionMiddleware.cs
@@ -0,0 +1,40 @@
+﻿using Newtonsoft.Json;
+using System.Net;
+
+namespace PruebaGeeksHubs.API.Exceptions
+{
+    public class ExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger _logger;
+
+        public ExceptionMiddleware(RequestDelegate next, ILoggerFactory loggerFactory)
+        {
+            _next = next;
+            _logger = loggerFactory.CreateLogger<ExceptionMiddleware>();
+        }
+
+        public async Task InvokeAsync(HttpContext httpContext)
+        {
+            try
+            {
+                await _next(httpContext);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, $"Something went wrong: {ex.Message}");
+                await HandleGlobalExceptionAsync(httpContext, ex);
+            }
+        }
+
+        private static Task HandleGlobalExceptionAsync(HttpContext context, Exception exception)
+        {
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int)HttpStatusCode.OK;
+
+            ErrorDetails errorDetails = new(StatusCodes.Status500InternalServerError, $"Error: {exception.Message}");
+
+            return context.Response.WriteAsync(JsonConvert.SerializeObject(errorDetails));
+        }
+    }
+}
