b4b86295aa5b0845a078cca3e92b57f47ad32485
Iván Jiménez González <ivanjimenez.contact@gmail.com>
2022-08-29 18:04:44 +0200

    Cadena conexión DB por Properties y separación Context-DB

--- PruebaGeeksHubs/PruebaGeeksHubs.Infrastructure/Contexts/TiendaDbContext.cs
+++ PruebaGeeksHubs/PruebaGeeksHubs.Infrastructure/Contexts/TiendaDbContext.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Metadata;
+﻿using Microsoft.EntityFrameworkCore;
 using PruebaGeeksHubs.Domain.Entities;
 
 namespace PruebaGeeksHubs.Infrastructure.Contexts
@@ -22,15 +19,6 @@ namespace PruebaGeeksHubs.Infrastructure.Contexts
         public virtual DbSet<Compra> Compras { get; set; } = null!;
         public virtual DbSet<CompraProducto> CompraProductos { get; set; } = null!;
         public virtual DbSet<Producto> Productos { get; set; } = null!;
-
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            if (!optionsBuilder.IsConfigured)
-            {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Server=localhost; Database=prueba-tecnica-geekshubs; Trusted_Connection=True; Connect Timeout=30;");
-            }
-        }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
---
+++ PruebaGeeksHubs/PruebaGeeksHubs.Infrastructure/ServiceRegistration.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using PruebaGeeksHubs.Infrastructure.Contexts;
+
+namespace PruebaGeeksHubs.Infrastructure
+{
+    public static class ServiceRegistration
+    {
+        public static void AddPersistenceInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<TiendaDbContext>(options => options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+
+            #region Repositories
+            #endregion Repositories
+        }
+    }
+}
